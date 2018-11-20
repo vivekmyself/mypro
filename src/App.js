2400eec789e3b4ffcfe5262a85a73e0bb85f5edb
Vivek Arockiaraj <vivek.arockiaraj@genomels.com>
2018-11-20 18:39:35 +0530

    Include reactstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import { Route, NavLink, HashRouter, Switch } from "react-router-dom";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import { Container, Row, Col, Nav, NavItem } from "reactstrap";
 import "./App.css";
 
 import Home from "./content/Home";
@@ -11,36 +13,45 @@ class App extends Component {
   render() {
     return (
       <HashRouter>
-        <div>
-          <h1>Simple SPA</h1>
-          <ul className="header">
-            <li>
-              <NavLink exact to="/">
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/stuff">Stuff</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact">Contact</NavLink>
-            </li>
-          </ul>
-          <div className="content">
-            <TransitionGroup>
-              <CSSTransition className="fade-enter" timeout={1000}>
-                <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/stuff" component={Stuff} />
-                  <Route path="/contact" component={Contact} />
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          </div>
-        </div>
+        <Container>
+          <Row>
+            <Col xs="6">
+              <h1>Simple SPA</h1>
+            </Col>
+            <Col xs="6">
+              <div>
+                <Nav className="header">
+                  <NavItem>
+                    <NavLink exact to="/">
+                      Home
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink to="/stuff">Stuff</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </NavItem>
+                </Nav>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="content">
+                <Route exact path="/" component={Home} />
+                <Route path="/stuff" component={Stuff} />
+                <Route path="/contact" component={Contact} />
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </HashRouter>
     );
   }
 }
+Container.propTypes = {
+  fluid: PropTypes.bool
+};
 
 export default App;
--- src/content/Contact.js
+++ src/content/Contact.js
@@ -1,14 +1,81 @@
 import React, { Component } from "react";
+import { Button, Popover, PopoverHeader, PopoverBody } from "reactstrap";
+
+class PopoverItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      popoverOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  }
+
+  render() {
+    return (
+      <span>
+        <Button
+          className="mr-1"
+          color="secondary"
+          id={"Popover-" + this.props.id}
+          onClick={this.toggle}
+        >
+          {this.props.item.text}
+        </Button>
+        <Popover
+          placement={this.props.item.placement}
+          isOpen={this.state.popoverOpen}
+          target={"Popover-" + this.props.id}
+          toggle={this.toggle}
+        >
+          <PopoverHeader>Popover Title</PopoverHeader>
+          <PopoverBody>
+            Sed posuere consectetur est at lobortis. Aenean eu leo quam.
+            Pellentesque ornare sem lacinia quam venenatis vestibulum.
+          </PopoverBody>
+        </Popover>
+      </span>
+    );
+  }
+}
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      popovers: [
+        {
+          placement: "top",
+          text: "Top"
+        },
+        {
+          placement: "bottom",
+          text: "Bottom"
+        },
+        {
+          placement: "left",
+          text: "Left"
+        },
+        {
+          placement: "right",
+          text: "Right"
+        }
+      ]
+    };
+  }
   render() {
     return (
       <div>
-        <h2>GOT QUESTIONS?</h2>
-        <p>
-          The easiest thing to do is post on our{" "}
-          <a href="http://www.google.com">forums</a>.
-        </p>
+        {this.state.popovers.map((popover, i) => {
+          return <PopoverItem key={i} item={popover} id={i} />;
+        })}
       </div>
     );
   }
